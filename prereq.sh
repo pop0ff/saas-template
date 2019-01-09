331784a5d7ecf0a3f311d78850f979bdb1ed28a2
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2019-01-09 16:50:42 +0200

    Delete prereq.sh

--- prereq.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/project-talan/talan-core/latest/install.sh)"
-sudo tln install java,docker,docker-compose,maven,nodejs
